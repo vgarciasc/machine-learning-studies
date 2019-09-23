89ff1356865165b543ec53dd19a71de429e93e90
vinigarcia <vingarciasc@gmail.com>
2019-09-23 20:51:18 -0300

    fix: sigmoid overflowing

--- ann/utils.py
+++ ann/utils.py
@@ -2,6 +2,7 @@ import numpy as np
 import math
 
 def sigmoid(x):
+    x = np.clip(x, -500, 500)
     return 1 / (1 + math.exp(-x))
 
 def dsigmoid(x):
@@ -18,3 +19,8 @@ def relu(x):
 
 def drelu(x):
     return 1 if x > 0 else 0
+
+def one_hot(size, peak):
+    output = np.zeros(size)
+    output[peak] = 1
+    return output
